d466982f3a2f121d8ae724a65d5ea85d84c9d029
toddlerCoder007 <167171420+toddlerCoder007@users.noreply.github.com>
2024-06-04 16:21:19 +0800

    Update q1-control-flow.js

--- assignment/q1-control-flow.js
+++ assignment/q1-control-flow.js
@@ -4,12 +4,22 @@
     - If `env` is "DEV", no change to `databaseCredential`.
     - If `env` is "STAGE", modify `databaseCredential` to "stageuser:password".
     - If `env` is "PROD", modify `databaseCredential` to "produser:password"
-    
+
 */
 
 const env = "DEV"; // Toggle between these possible values "DEV" | "STAGE" | "PROD"
 let databaseCredential = "devuser:password";
 
 // Task: Add code here
+switch (env) {
+    case 'DEV' :
+        databaseCredential = "devuser:password";
+    case 'STAGE' :
+        databaseCredential = "stageuser:password";
+    case'PROD' :
+        databaseCredential = "produser:password";
+    default :
+        return null;
+}
 
 console.log(`Database credential for environment ${env} is ${databaseCredential}`);
